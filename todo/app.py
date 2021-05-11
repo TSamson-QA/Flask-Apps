3bbf75275945666c3b4355bca1eb7e5d117aa4f7
Tom-Samson <TSamson@qa.com>
2021-05-11 17:09:23 +0100

    fixed todo, still WIP

--- todo/app.py
+++ todo/app.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def home():
     return 'This is my TODO list'
 
+
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@34.105.221.153:3306/flask_instance"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) 
@@ -16,6 +19,13 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(30), nullable=False)
     complete = db.Column(db.Boolean, nullable=False)
+
+#WIP adding Todo page
+@app.route('/todo')
+def todo():
+    all_todo = Todo.query.all()
+    return str(all_todo)
+    #find way to print/return entries from table?
 
 if __name__=='__main__':
     app.run(debug=True, host='127.0.0.1')
--- todo/create.py
+++ todo/create.py
@@ -1,4 +1,10 @@
-from app import db
+from app import db, Todo
 
 db.drop_all()
 db.create_all() # Creates all table classes defined
+
+
+new_todo = Todo(task="added item to todo list",complete=0)
+
+db.session.add(new_todo)
+db.session.commit()
