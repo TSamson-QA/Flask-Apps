7adafc80eb19f3df8e75f2a54d01804d46433af7
Tom-Samson <TSamson@qa.com>
2021-05-13 12:16:54 +0100

    fixed testing

--- flask_test/tests/test_app.py
+++ flask_test/tests/test_app.py
@@ -5,26 +5,41 @@ from flask_testing import TestCase
 # import the app's classes and objects
 from app import app, db, Register
 
+# Create the base class
 class TestBase(TestCase):
     def create_app(self):
 
-        app.config.update(SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@34.105.221.153:3306/flask_instance",
-        SECRET_KEY='TEST_SECRET_KEY', debug=True, WTF_CSRF_ENABLED=False)
+        # Pass in testing configurations for the app. Here we use sqlite without a persistent database for our tests.
+        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
+                SECRET_KEY='TEST_SECRET_KEY',
+                DEBUG=True,
+                WTF_CSRF_ENABLED=False
+                )
         return app
 
     def setUp(self):
-
+        """
+        Will be called before every test
+        """
+        # Create table
         db.create_all()
+
+        # Create test registree
         sample1 = Register(name="MissWoman")
 
+        # save users to database
         db.session.add(sample1)
         db.session.commit()
 
-def tearDown(self):
+    def tearDown(self):
+        """
+        Will be called after every test
+        """
 
-    db.session.remove()
-    db.drop_all()
+        db.session.remove()
+        db.drop_all()
 
+# Write a test class for testing that the home page loads but we are not able to run a get request for delete and update routes.
 class TestViews(TestBase):
 
     def test_home_get(self):
@@ -32,6 +47,7 @@ class TestViews(TestBase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'MissWoman', response.data)
 
+# Test adding 
 class TestAdd(TestBase):
     def test_add_post(self):
         response = self.client.post(
